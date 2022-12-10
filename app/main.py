54dd1f74c025d233030f86f2d12cb379849c236f
emm331 <emm331@georgetown.edu>
2022-12-09 19:03:12 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,10 +8,10 @@ import app.tajMusic as tm
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+#load_dotenv()
 
-client_id = os.getenv("client_id")
-client_secret = os.getenv("client_secret")
+#client_id = os.getenv("client_id")
+#client_secret = os.getenv("client_secret")
 #client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_id=client_secret)
 #sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 #client_credentials_manager = os.getenv("client_credentials_manager")
